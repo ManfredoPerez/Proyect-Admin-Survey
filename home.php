e57288fa44825471ef3bd405e70b2a60ad8eeeac
Edvin Canastuj <ecanastujv@miumg.edu.gt>
2023-05-11 14:55:57 -0600

    cambiando la configuración para que la tabla usuarios solo se vea en Admin

--- home.php
+++ home.php
@@ -205,44 +205,13 @@ include('db_connect.php');
   data-mdb-dataset-data="[2112, 2343, 2545, 3423, 2365, 1985, 987]"
   data-mdb-dataset-background-color="['rgba(63, 81, 181, 0.5)', 'rgba(77, 182, 172, 0.5)', 'rgba(66, 133, 244, 0.5)', 'rgba(156, 39, 176, 0.5)', 'rgba(233, 30, 99, 0.5)', 'rgba(66, 73, 244, 0.4)', 'rgba(66, 133, 244, 0.2)']"
 ></canvas>
-
-<?php else: ?>
-	 <div class="col-12">
-          <div class="card">
-          	<div class="card-body">
-          		Bienvenido <?php echo $_SESSION['login_name'] ?>!
-          	</div>
-          </div>
-      </div>
-      <div class="row">
-          <div class="col-12 col-sm-6 col-md-3">
-
-            <div class="small-box bg-gradient-success">
-              <div class="inner">
-                <h3>
-                  <?php echo $conn->query("SELECT distinct(survey_id) FROM answers  where user_id = {$_SESSION['login_id']}")->num_rows; ?>
-                </h3>
-                <p>Total de encuestas realizadas</p>
-              </div>
-              <div class="icon">
-                <i class="fas fa-poll-h"></i>
-              </div>
-            
-            </div>
-
-            <!-- /.info-box -->
-          </div>
-          <!-- /.col -->
-      </div>
-      
-<?php endif; ?>
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="assets/dist/css/tabla.css">
   <div class="table-container">
   <h2 class="table-title">Información de usuarios</h2>
   <div class="table-responsive-sm">
-  <?php
+  <?php 
 $sql = "SELECT firstname, contact, email, type FROM users";
 $result = $conn->query($sql);
 // Crear la tabla HTML
@@ -277,5 +246,36 @@ $conn->close();
 <?php  ?>
 </div>
 </head>
+<?php else: ?>
+	 <div class="col-12">
+          <div class="card">
+          	<div class="card-body">
+          		Bienvenido <?php echo $_SESSION['login_name'] ?>!
+          	</div>
+          </div>
+      </div>
+      <div class="row">
+          <div class="col-12 col-sm-6 col-md-3">
+
+            <div class="small-box bg-gradient-success">
+              <div class="inner">
+                <h3>
+                  <?php echo $conn->query("SELECT distinct(survey_id) FROM answers  where user_id = {$_SESSION['login_id']}")->num_rows; ?>
+                </h3>
+                <p>Total de encuestas realizadas</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-poll-h"></i>
+              </div>
+            
+            </div>
+
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+      </div>
+      
+<?php endif; ?>
+
 
 
